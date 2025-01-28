28625da083c83960857c83a8d888abd416c93d55
René Elizalde <reroes799@gmail.com>
2025-01-28 17:36:43 -0500

    ejercicio

---
+++ ejemplo0.py
@@ -0,0 +1,21 @@
+"""
+    Ejemplo de estructura de condicional
+    simple
+"""
+# variable edad, asume el valor ingresado porteclado
+edad = input("Ingrese la edad de la persona: ")
+
+# la variable edad, asume un valor de tipo cadena
+# por defecto con la función input
+# Se debe hacer un cambio de tipo de dato para que
+# sea considerado con entero, a través de int
+
+edad = int(edad)
+
+# desde aquí la variable edad es considerada con tipo d
+# de dato entero
+
+if edad >= 18:
+    print("La persona es mayor de edad en Ecuador")
+
+
